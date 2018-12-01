b131caeac302d0617bfb46357a273b1105a936d8
deadlockk <style8914@naver.com>
2018-12-02 07:12:20 +0900

    update match activity

--- mobile/src/main/java/com/github/pocmo/sensordashboard/MatchActivity.java
+++ mobile/src/main/java/com/github/pocmo/sensordashboard/MatchActivity.java
@@ -10,33 +10,73 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class MatchActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
     ArrayList<User> userArrayList = new ArrayList<>();
     private String betContents;
     private String yourEmail;
     private String myEmail;
-
+    Callback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+        loadData("user_info", callback);
+    }
+
+    public interface Callback{
+        void success(String data);
+        void fail(String errorMessage);
+    }
+
+    private DatabaseReference mDatabase= null;
+
+    public void loadData(String path, final Callback callback){
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        StringTokenizer token= new StringTokenizer(path, "/", false);
+        while(token.hasMoreTokens()){
+            mDatabase= mDatabase.child(token.nextToken());
+        }
+
+        // Read from the database
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.success(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                callback.fail(error.getMessage());
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        ImageView myProfile = (ImageView)findViewById(R.id.myProfile);
+        ImageView myProfile = (ImageView) findViewById(R.id.myProfile);
         Glide.with(this).load(user.getPhotoUrl().toString()).into(myProfile); //프로필 사진 보여주기
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        userArrayList = (ArrayList<User>)getIntent().getSerializableExtra("userList");
+        userArrayList = (ArrayList<User>) getIntent().getSerializableExtra("userList");
         Intent intent = getIntent();
         betContents = intent.getExtras().getString("betContents");
 
@@ -44,30 +84,39 @@ public class MatchActivity extends AppCompatActivity {
         Iterator<User> userIterator = userArrayList.iterator();
         while (userIterator.hasNext()) {
             User tempUser = userIterator.next();
-            if(tempUser.getUsername().equals(user.getUid())) {
+            if (tempUser.getUsername().equals(user.getUid())) {
                 if (betContents.equals("null")) {
                     betContents = tempUser.getBetting();
+
                 }
                 yourUid = tempUser.getIsVS();
+
+                Log.e("제발000", tempUser.getEmail() + tempUser.getIsVS() + tempUser.getBetting() + tempUser.getUsername());
+
                 myEmail = tempUser.getEmail();
                 break;
             }
         }
-        while (userIterator.hasNext()) {
-            User tempUser = userIterator.next();
-            if (tempUser.getUsername().equals(yourUid)) {
-                yourEmail = tempUser.getEmail();
+
+        userArrayList = (ArrayList<User>) getIntent().getSerializableExtra("userList");
+        Iterator<User> userIteratorr = userArrayList.iterator(); // while 문 안들어가길래 걍 하나 더 만듦
+        while (userIteratorr.hasNext()) {
+            User tempUserr = userIteratorr.next();
+
+            if (tempUserr.getUsername().equals(yourUid)) {
+                yourEmail = tempUserr.getEmail();
                 break;
             }
         }
 
-        TextView yourID = (TextView)findViewById(R.id.yourID);
-        yourID.setText(yourEmail.split("@")[0]);
+        TextView yourID = (TextView) findViewById(R.id.yourID);
+//        yourID.setText(yourEmail.split("@")[0]);
 
-        TextView myID = (TextView)findViewById(R.id.myID);
+        TextView myID = (TextView) findViewById(R.id.myID);
         myID.setText(myEmail.split("@")[0]);
-        TextView contents = (TextView)findViewById(R.id.contents);
+        TextView contents = (TextView) findViewById(R.id.contents);
         contents.setText(betContents);
 
     }
+
 }
--- mobile/src/main/java/com/github/pocmo/sensordashboard/VSmode.java
+++ mobile/src/main/java/com/github/pocmo/sensordashboard/VSmode.java
@@ -1,6 +1,11 @@
 package com.github.pocmo.sensordashboard;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -10,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +44,10 @@ public class VSmode extends AppCompatActivity {
     static EditText betContents;
     private Button btn;
     private DatabaseReference mDatabase;
+
+
+    ProgressBar loginProgress;
+
     ArrayList<User> userArrayList = new ArrayList<>();
 
     @Override
@@ -49,50 +59,92 @@ public class VSmode extends AppCompatActivity {
         myEmail = user.getEmail();
         searchEmail = findViewById(R.id.searchEmail);
         betContents = findViewById(R.id.betContents);
+        loginProgress = (ProgressBar) findViewById(R.id.loginProgress);
+
         btn = findViewById(R.id.complete);
         Log.e("가나다", "" + myTokenID + ", " + myEmail);
 
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        userArrayList = (ArrayList<User>)getIntent().getSerializableExtra("userList");
+        userArrayList = (ArrayList<User>) getIntent().getSerializableExtra("userList");
         Iterator<User> userIterator = userArrayList.iterator();
         while (userIterator.hasNext()) {
             User tempUser = userIterator.next();
             Log.e("제발1", tempUser.getIsVS() + " / " + myTokenID);
-            if(tempUser.getIsVS().equals(myTokenID)) {
-                Intent intent = new Intent(getApplicationContext(), MatchActivity.class);
-                intent.putExtra("userList", userArrayList);
-                intent.putExtra("betContents", "null");
-                startActivity(intent);
-                finish();
-                break;
+            if (tempUser.getIsVS() != null) {// null 때문에 안돌아가서 null 이 아닐 때만
+                if (tempUser.getIsVS().equals(myTokenID)) {
+                    Intent intent = new Intent(getApplicationContext(), MatchActivity.class);
+                    intent.putExtra("userList", userArrayList);
+                    intent.putExtra("betContents", "null");
+                    startActivity(intent);
+                    finish();
+                    break;
+                }
             }
             Log.e("제발4", tempUser.getEmail() + " / " + tempUser.getIsVS());
         }
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Iterator<User> checkEmail = userArrayList.iterator();
+                int a =0;
                 while (checkEmail.hasNext()) {
                     User tempUser = checkEmail.next();
                     if ((searchEmail.getText().toString()).equals(tempUser.getEmail())) {
-                        Log.e("제발", searchEmail.getText() + "이메일이 있습니다");
+                        a=1;
+                        showProgress(true);
                         mDatabase.child("user_info").child(myTokenID).child("isVS").setValue(tempUser.getUsername());
                         mDatabase.child("user_info").child(myTokenID).child("betting").setValue(betContents.getText().toString());
                         mDatabase.child("user_info").child(tempUser.getUsername()).child("isVS").setValue(myTokenID);
                         mDatabase.child("user_info").child(tempUser.getUsername()).child("betting").setValue(betContents.getText().toString());
-                        Intent intent = new Intent(getApplicationContext(), MatchActivity.class);
-                        intent.putExtra("betContents", betContents.getText().toString());
-                        intent.putExtra("userList", userArrayList);
-                        startActivity(intent);
-                        finish();
-                        break;
+
+                        Log.e("제발 디비","확인확인");
+                        Handler timer = new Handler(); //Handler 생성
+                        timer.postDelayed(new Runnable() { //2초후 쓰레드를 생성하는 postDelayed 메소드
+                            public void run() {
+                                Intent intent = new Intent(getApplicationContext(), MatchActivity.class);
+                                intent.putExtra("betContents", betContents.getText().toString());
+                                intent.putExtra("userList", userArrayList);
+                                startActivity(intent); //다음 액티비티 이동
+                                finish(); // 이 액티비티를 종료
+                                showProgress(false);
+                            }
+                        }, 2000); //2000은 2초를 의미한다.
+
                     }
                 }
-                Toast.makeText(getApplicationContext(), "Please, enter the available e-mail",Toast.LENGTH_LONG).show();// 유효한 이메일 검색이 아닐 때
+                if(a==0)
+                Toast.makeText(getApplicationContext(), "Please, enter the available e-mail", Toast.LENGTH_LONG).show();// 유효한 이메일 검색이 아닐 때
             }
         });
+    }
+
+    /**
+     * Progressing view
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            loginProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+            loginProgress.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    loginProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            loginProgress.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
     }
 }
